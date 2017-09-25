05995f74f785c98f6055f9960d33da0323da3da9
kenystev <kjestevez.0795@gmail.com>
2017-09-25 17:26:10 -0600

    bit expressions

--- tinyC/src/ast/bits_expression.cpp
+++ tinyC/src/ast/bits_expression.cpp
@@ -1,36 +1,71 @@
 #include "bits_expression.h"
 
-void ShiftLeftExpr::genCode(codeData &cd)
+void ShiftLeftExpr::genCode(codeData &data)
+{
+	codeData le, re;
+	expr1->genCode(le);
+	expr2->genCode(re);
+	releaseTemp(le.place);
+	releaseTemp(re.place);
+	data.place = nextTemp();
+
+	data.code = "sll " + data.place + ", " + le.place + ", " + re.place + "\n";
+}
+
+void ShiftRightExpr::genCode(codeData &data)
+{
+	codeData le, re;
+	expr1->genCode(le);
+	expr2->genCode(re);
+	releaseTemp(le.place);
+	releaseTemp(re.place);
+	data.place = nextTemp();
+
+	data.code = "srl " + data.place + ", " + le.place + ", " + re.place + "\n";
+}
+
+void BitAndExpr::genCode(codeData &data)
+{
+	codeData le, re;
+	expr1->genCode(le);
+	expr2->genCode(re);
+	releaseTemp(le.place);
+	releaseTemp(re.place);
+	data.place = nextTemp();
+
+	data.code = "and " + data.place + ", " + le.place + ", " + re.place + "\n";
+}
+
+void XorExpr::genCode(codeData &data)
+{
+	codeData le, re;
+	expr1->genCode(le);
+	expr2->genCode(re);
+	releaseTemp(le.place);
+	releaseTemp(re.place);
+	data.place = nextTemp();
+
+	data.code = "xor " + data.place + ", " + le.place + ", " + re.place + "\n";
+}
+
+void BitOrExpr::genCode(codeData &data)
+{
+	codeData le, re;
+	expr1->genCode(le);
+	expr2->genCode(re);
+	releaseTemp(le.place);
+	releaseTemp(re.place);
+	data.place = nextTemp();
+
+	data.code = "or " + data.place + ", " + le.place + ", " + re.place + "\n";
+}
+
+void AndExpr::genCode(codeData &data)
 {
 	
 }
 
-void ShiftRightExpr::genCode(codeData &cd)
-{
-	
-}
-
-void BitAndExpr::genCode(codeData &cd)
-{
-	
-}
-
-void XorExpr::genCode(codeData &cd)
-{
-	
-}
-
-void BitOrExpr::genCode(codeData &cd)
-{
-	
-}
-
-void AndExpr::genCode(codeData &cd)
-{
-	
-}
-
-void OrExpr::genCode(codeData &cd)
+void OrExpr::genCode(codeData &data)
 {
 	
 }
--- tinyC/src/ast/unary_expression.cpp
+++ tinyC/src/ast/unary_expression.cpp
@@ -2,12 +2,26 @@
 
 void PreIncExpr::genCode(codeData &cd)
 {
+	string id = ((IdExpr*)expr)->id;
+	data.place = nextTemp();
+	string place = nextTemp();
 
+	data.code = "\tlw " + place + ", " + id + "\n";
+	data.code += "\taddi " + data.place + ", " + data.place + ", 1\n";
+	data.code += "\tsw " + data.place + ", " + id + "\n";
+	releaseTemp(place);
 }
 
 void PreDecExpr::genCode(codeData &cd)
 {
+	string id = ((IdExpr*)expr)->id;
+	data.place = nextTemp();
+	string place = nextTemp();
 
+	data.code = "\tlw " + place + ", " + id + "\n";
+	data.code += "\taddi " + data.place + ", " + data.place + ", -1\n";
+	data.code += "\tsw " + data.place + ", " + id + "\n";
+	releaseTemp(place);
 }
 
 void PositiveNumberExpr::genCode(codeData &cd)
@@ -22,7 +36,11 @@ void NegativeNumberExpr::genCode(codeData &cd)
 
 void BitNotExpr::genCode(codeData &cd)
 {
+	string id = ((IdExpr*)expr)->id;
+	data.place = nextTemp();
 
+	data.code += "\txori " + data.place + ", 0xffff\n";
+	releaseTemp(place);
 }
 
 void LogicNotExpr::genCode(codeData &cd)
